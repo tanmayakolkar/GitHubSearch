79d5205fb54a6d0c9640f3c46c14cce3e49b3b0c
Tanmay Akolkar <mail.tony24@gmail.com>
2022-01-20 12:13:21 +0530

    i have added new file in to GitHub Serach Project

---
+++ src/components/New.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class New extends React.Component
+{
+    constructor(props)
+    {
+        super(props)
+    }
+    render()
+    {
+        return(
+            <React.Fragment>
+                <h1>Hello this is new file add in to Component</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default New
