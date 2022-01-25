b9149804f3b06e5ca430d457b552c5fc45523c0a
Tanmay Akolkar <mail.tony24@gmail.com>
2022-01-25 17:17:54 +0530

    I have added Display Como in to Project by Using Tanmay/show branch

---
+++ src/components/Display.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Display extends React.Component
+{
+    constructor(props)
+    {
+        super(props)
+    }
+    render()
+    {
+        return(
+            <React.Fragment>
+                <h1>Hello This is Display Component</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Display
